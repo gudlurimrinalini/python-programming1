5812c8d3e153a81de3f22c11c46097a6725ae412
gudlurimrinalini <42641536+gudlurimrinalini@users.noreply.github.com>
2018-08-24 16:08:14 +0530

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,5 +1,7 @@
 i=int(raw_input())
-if(i%2)==0:
-	print("number is even")
+if((i%2)==0):
+	print("number is Even")
+elif((i%2)!=0):
+	print("number is Odd")
 else:
-	print("number is odd")
+	print("Invalid")
